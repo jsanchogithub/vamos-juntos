a8350d3966501966a75a40ce4fbb966cd87a0b31
Jose Antonio Garcia <jose.a.g.sancho@gmail.com>
2025-09-25 18:47:44 +0100

    c20250925 1847

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,11 @@ import Dashboard from './pages/Dashboard';
 function App() {
 
   const location = useLocation();
+  const jgsArr1 = ['jgs1110','jgs1111','jgs1112']; //jgs20250925
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random()*(max+1));
+  }
 
   useEffect(() => {
     document.querySelector('html').style.scrollBehavior = 'auto'
@@ -30,6 +35,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Dashboard />} />
       </Routes>
+      {/*{jgsArr1[getRandomInt(2)]}  aparece no fim*/}
     </>
   );
 }
