c735a077882ed44a6238c8b8276d20c8dae7aefb
Jose Antonio Garcia <jose.a.g.sancho@gmail.com>
2025-09-27 15:49:50 +0100

    sidebar siempre visible

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -58,11 +58,22 @@ function Sidebar({
       ></div>
 
       {/* Sidebar */}
+      {/* Sidebar jgs20250927, para conseguir mostrar sidebar c scroll, solo 80% 
+
       <div
         id="sidebar"
         ref={sidebar}
         className={`flex lg:flex! flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-[100dvh] overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:w-64! shrink-0 bg-white dark:bg-gray-800 p-4 transition-all duration-200 ease-in-out ${sidebarOpen ? "translate-x-0" : "-translate-x-64"} ${variant === 'v2' ? 'border-r border-gray-200 dark:border-gray-700/60' : 'rounded-r-2xl shadow-xs'}`}
       >
+      */}
+
+      <div
+        id="sidebar"
+        ref={sidebar}
+        className={`flex lg:flex! flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:w-64! shrink-0 bg-white dark:bg-gray-800 p-4 transition-all duration-200 ease-in-out ${sidebarOpen ? "translate-x-0" : "-translate-x-64"} ${variant === 'v2' ? 'border-r border-gray-200 dark:border-gray-700/60' : 'rounded-r-2xl shadow-xs'}`}
+        style={{ height: "calc(100dvh - 20%)" }} // le restamos el 20% al alto
+      >
+
         {/* Sidebar header */}
         <div className="flex justify-between mb-10 pr-3 sm:px-2">
           {/* Close button */}
