dd14ed0d1ddd855ba1f7c53839224b1d10e4c96c
Jose Antonio Garcia <jose.a.g.sancho@gmail.com>
2025-09-27 12:57:10 +0100

    margenes ok

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -44,7 +44,8 @@ function Dashboard() {
 
           {/* jgs20250927 <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">  */}
           {/* Forzamos que todo el contenido solo ocupe 90% del ancho */}
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-[90vw] max-w-[90vw] mx-auto">
+          {/* <div className="px-4 sm:px-6 lg:px-8 py-8 w-[90vw] max-w-[90vw] mx-auto"> */}
+           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-[90vw]">
 
             {/* Dashboard actions */}
             <div className="sm:flex sm:justify-between sm:items-center mb-8">
@@ -55,20 +56,21 @@ function Dashboard() {
               </div>
 
               {/* Right: Actions */}
-              <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-                {/* Filter button */}
+              <div className="flex flex-wrap justify-end gap-2 max-w-full">
                 <FilterButton align="right" />
-                {/* Datepicker built with React Day Picker */}
                 <Datepicker align="right" />
-                {/* Add view button */}
                 <button className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white">
-                  <svg className="fill-current shrink-0 xs:hidden" width="16" height="16" viewBox="0 0 16 16">
+                  <svg
+                    className="fill-current shrink-0 xs:hidden"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                  >
                     <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
                   </svg>
                   <span className="max-xs:sr-only">Add View</span>
-                </button>                
+                </button>
               </div>
-
             </div>
 
             {/* Cards */}
