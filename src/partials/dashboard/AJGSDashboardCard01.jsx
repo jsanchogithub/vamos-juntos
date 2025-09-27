919ed1af9b53943f440a798077f43dc6eeaa37b2
Jose Antonio Garcia <jose.a.g.sancho@gmail.com>
2025-09-27 18:23:40 +0100

    foto tierra

--- src/partials/dashboard/AJGSDashboardCard01.jsx
+++ src/partials/dashboard/AJGSDashboardCard01.jsx
@@ -22,7 +22,7 @@ function AJGSDashboardCard01() {
   return (
     <div className="flex flex-col col-span-full sm:col-span-12 bg-white dark:bg-gray-800 shadow-xs rounded-xl h-[80vh] overflow-hidden">
       <header className="px-5 py-4 border-b border-gray-100 dark:border-gray-700/60 flex items-center">
-        <h2 className="font-semibold text-gray-800 dark:text-gray-100">Tierra Giratoria<./h2>
+        <h2 className="font-semibold text-gray-800 dark:text-gray-100">Tierra Giratoria.</h2>
       </header>
       <div className="flex-1">
         <Globe
